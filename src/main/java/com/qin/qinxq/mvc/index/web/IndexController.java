d07fa08111dabd33160fdd8780022ba2a31bfe1f
qinxq <qinxiaoqiangme.gmail.com>
2016-10-26 18:15:27 +0800

    2016/10/26

--- src/main/java/com/qin/qinxq/mvc/index/web/IndexController.java
+++ src/main/java/com/qin/qinxq/mvc/index/web/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by qinxq on 2016/10/23 21:49
  */
 @Controller
+@RequestMapping(value = "index")
 public class IndexController {
     @RequestMapping(value = "index")
     public String toIndex(){
